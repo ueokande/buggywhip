f625b23989d8e4b159338a61b837a30bf08c2359
Shin'ya Ueoka <ueokande@i-beam.org>
2017-01-04 11:32:19 +0900

    Define commands

--- src/main.c
+++ src/main.c
@@ -14,10 +14,20 @@ void do_readline() {
 
     if (strcmp_eq(line, "quit")) {
       break;
+    } else if (strcmp_eq(line, "next")) {
+    } else if (strcmp_eq(line, "help")) {
+    } else if (strcmp_eq(line, "list")) {
+    } else if (strcmp_eq(line, "continue")) {
+    } else if (strcmp_eq(line, "edit")) {
+    } else if (strcmp_eq(line, "step")) {
+    } else if (strcmp_eq(line, "print")) {
+    } else if (strcmp_eq(line, "run")) {
+    } else if (strcmp_eq(line, "backtrace")) {
+    } else {
+      fprintf(stdout, "Undefined command: \"%s\"\n",  line);
     }
-
-    free(line);
   }
+  free(line);
 }
 
 int main(void) {
