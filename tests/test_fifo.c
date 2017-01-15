57ee4aeee9cbd70dd791d048bb24e3762657e69e
Shin'ya Ueoka <ueokande@i-beam.org>
2017-01-15 12:00:21 +0900

    Fix indents

--- tests/test_fifo.c
+++ tests/test_fifo.c
@@ -7,36 +7,36 @@
 #include "fifo.h"
 
 START_TEST(test_create_fifo) {
-        struct bgw_fifo fifo;
-        struct stat fifo_st, dir_st;
-        char dir[256];
+	struct bgw_fifo fifo;
+	struct stat fifo_st, dir_st;
+	char dir[256];
 
-        create_fifo(&fifo);
-        strcpy(dir, fifo.name);
-        dirname(dir);
+	create_fifo(&fifo);
+	strcpy(dir, fifo.name);
+	dirname(dir);
 
-        stat(fifo.name, &fifo_st);
-        stat(dir, &dir_st);
+	stat(fifo.name, &fifo_st);
+	stat(dir, &dir_st);
 
-        ck_assert(fifo_st.st_mode & S_IFIFO);
-        ck_assert(dir_st.st_mode & S_IFDIR);
-        
-        remove_fifo(&fifo);
+	ck_assert(fifo_st.st_mode & S_IFIFO);
+	ck_assert(dir_st.st_mode & S_IFDIR);
+
+	remove_fifo(&fifo);
 }
 END_TEST
 
 START_TEST(test_remove_fifo) {
-        struct bgw_fifo fifo;
-        struct stat fifo_st, dir_st;
-        char dir[256];
+	struct bgw_fifo fifo;
+	struct stat fifo_st, dir_st;
+	char dir[256];
 
-        create_fifo(&fifo);
-        remove_fifo(&fifo);
+	create_fifo(&fifo);
+	remove_fifo(&fifo);
 
-        strcpy(dir, fifo.name);
-        dirname(dir);
+	strcpy(dir, fifo.name);
+	dirname(dir);
 
-        ck_assert(stat(fifo.name, &fifo_st) < 0);
-        ck_assert(stat(dir, &dir_st) < 0);
+	ck_assert(stat(fifo.name, &fifo_st) < 0);
+	ck_assert(stat(dir, &dir_st) < 0);
 }
 END_TEST
