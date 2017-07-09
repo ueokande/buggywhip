38b3616db0945f814ddbfa59f4cb3cb00583f38b
Shin'ya Ueoka <ueokande@i-beam.org>
2017-07-09 11:38:29 +0900

    add load command

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 
 	"github.com/chzyer/readline"
 )
 
+var context struct {
+	stderr io.Writer
+	stdout io.Writer
+
+	source string
+}
+
+func listFiles(path string) func(string) []string {
+	return func(line string) []string {
+		files, _ := ioutil.ReadDir(path)
+		names := make([]string, 0, len(files))
+		for _, f := range files {
+			names = append(names, f.Name())
+		}
+		return names
+	}
+}
+
 var completer = readline.NewPrefixCompleter(
 	readline.PcItem("help"),
 	readline.PcItem("exit"),
 
+	readline.PcItem("load", readline.PcItemDynamic(listFiles("./"))),
 	readline.PcItem("list"),
 	readline.PcItem("run"),
 	readline.PcItem("step"),
@@ -26,6 +47,7 @@ func cmdHelp(w io.Writer) (int, error) {
 
 help -- Print list of commands
 exit -- Exit buggywhip
+load -- Load source
 list -- List source from specified line of keyword
 run -- Start debugged script
 step -- Step program line by line
@@ -34,24 +56,23 @@ breakpoint -- Manage breakpoints
 `)
 }
 
-func run() int {
-	l, err := readline.NewEx(&readline.Config{
-		Prompt:          "(bgw) ",
-		AutoComplete:    completer,
-		InterruptPrompt: "^C",
-		EOFPrompt:       "exit",
-
-		HistorySearchFold: true,
-	})
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return 1
+func cmdLoad(args []string) error {
+	if len(args) == 0 {
+		return errors.New("no files specified")
 	}
-	defer l.Close()
+	source := args[0]
+	_, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+	context.source = source
+	return nil
+}
 
+func readlineLoop(rl *readline.Instance) {
 readline_loop:
 	for {
-		line, err := l.Readline()
+		line, err := rl.Readline()
 		if err == readline.ErrInterrupt {
 			continue
 		} else if err == io.EOF {
@@ -65,23 +86,43 @@ readline_loop:
 
 		switch fields[0] {
 		case "help":
-			cmdHelp(l.Stderr())
-		case "list":
-			io.WriteString(l.Stderr(), "list command not implemented yet\n")
-		case "run":
-			io.WriteString(l.Stderr(), "run command not implemented yet\n")
-		case "step":
-			io.WriteString(l.Stderr(), "step command not implemented yet\n")
-		case "next":
-			io.WriteString(l.Stderr(), "next command not implemented yet\n")
-		case "breakpoint":
-			io.WriteString(l.Stderr(), "breakpoint command not implemented yet\n")
+			cmdHelp(rl.Stderr())
+		case "list", "run", "step", "next", "breakpoint":
+			err = errors.New("command not implemented: " + fields[0])
+		case "load":
+			err = cmdLoad(fields[1:])
 		case "exit":
 			break readline_loop
 		default:
-			io.WriteString(l.Stderr(), "Unknown command: "+fields[0]+"\n")
+			err = errors.New("unknown command: " + fields[0] + "")
+		}
+		if err != nil {
+			fmt.Fprintln(rl.Stderr(), err.Error())
 		}
 	}
+
+}
+
+func run() int {
+	if len(os.Args) > 1 {
+		context.source = os.Args[1]
+	}
+
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "(bgw) ",
+		AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+
+		HistorySearchFold: true,
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	defer rl.Close()
+
+	readlineLoop(rl)
 
 	return 0
 }
