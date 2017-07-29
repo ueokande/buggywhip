a936027b8b947a935800690e2c320073df80b7dd
Shin'ya Ueoka <ueokande@i-beam.org>
2017-07-29 10:22:09 +0900

    run command as interface

--- run.go
+++ run.go
@@ -3,22 +3,39 @@ package main
 import (
 	"bufio"
 	"errors"
+	"io"
 	"os"
 )
 
-func cmdRun(args []string) error {
-	if len(context.source) == 0 {
+type runContext struct {
+	source string
+	stdout io.Writer
+	stderr io.Writer
+	w      func(string)
+}
+
+func newRunContext(source string, stdout io.Writer, stderr io.Writer, writer func(string)) (*runContext, error) {
+	return &runContext{
+		source: source,
+		stdout: stdout,
+		stderr: stderr,
+		w:      writer,
+	}, nil
+}
+
+func (c *runContext) run(args []string) error {
+	if len(c.source) == 0 {
 		return errors.New("file not loaded")
 	}
 
-	f, err := os.Open(context.source)
+	f, err := os.Open(c.source)
 	if err != nil {
 		return err
 	}
 
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-		context.ch <- s.Text() + "\n"
+		c.w(s.Text() + "\n")
 	}
 	return nil
 }
